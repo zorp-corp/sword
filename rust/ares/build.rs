0c56775dbebba820e34140c0b4c667510363d130
Alex Shelkovnykov <a@ashelkov.com>
2023-05-20 23:35:31 +0000

    build: split build.rs profiles

--- rust/ares/build.rs
+++ rust/ares/build.rs
@@ -1,11 +1,58 @@
 fn main() {
+    use std::env;
+    let profile = env::var("PROFILE").unwrap();
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=./src/pma");
+
+    match profile.as_ref() {
+        "debug" => debug(),
+        "release" => release(),
+        _ => {
+            println!("unknown profile: {}", profile);
+            std::process::exit(-1);
+        }
+    }
+}
+
+fn debug() {
     cc::Build::new()
         .file("./src/pma/malloc.c")
         .file("./src/pma/includes/checksum.c")
-        // .opt_level(3)
         .opt_level(0)
         .flag("-g3")
+        .flag("-Wall")
+        .flag("-Wextra")
+        .flag("-Wpedantic")
+        .flag("-Wformat=2")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wshadow")
+        .flag("-Wwrite-strings")
+        .flag("-Wstrict-prototypes")
+        .flag("-Wold-style-definition")
+        .flag("-Wredundant-decls")
+        .flag("-Wnested-externs")
+        .flag("-Wmissing-include-dirs")
+        .compile("pma_malloc");
+}
+
+fn release() {
+    cc::Build::new()
+        .file("./src/pma/malloc.c")
+        .file("./src/pma/includes/checksum.c")
+        .warnings_into_errors(true)
+        .opt_level(3)
+        .flag("-Wall")
+        .flag("-Wextra")
+        .flag("-Wpedantic")
+        .flag("-Wformat=2")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wshadow")
+        .flag("-Wwrite-strings")
+        .flag("-Wstrict-prototypes")
+        .flag("-Wold-style-definition")
+        .flag("-Wredundant-decls")
+        .flag("-Wnested-externs")
+        .flag("-Wmissing-include-dirs")
         .compile("pma_malloc");
 }
