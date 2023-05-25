1a4d5b15ec310a606b1300a805daa28c569758c2
Alex Shelkovnykov <a@ashelkov.com>
2023-05-25 13:43:05 +0000

    pma: fix void* arithmetic warnings

--- rust/ares/src/pma/malloc.c
+++ rust/ares/src/pma/malloc.c
@@ -1605,7 +1605,7 @@ _pma_get_cached_pages(uint64_t num_pages) {
     // If run larger than necessary by two pages...
     if (valid_page_run->length > (num_pages + 1)) {
       // Reduce it
-      valid_page_run->page += (num_pages * PMA_PAGE_SIZE);
+      valid_page_run->page = (uint8_t*)valid_page_run->page + (num_pages * PMA_PAGE_SIZE);
       valid_page_run->length -= num_pages;
 
     // Otherwise...
@@ -1676,7 +1676,7 @@ _pma_get_new_page(PMAPageStatus status) {
   assert(address == _pma_state->metadata->arena_end);
 
   // Record PMA expansion
-  _pma_state->metadata->arena_end += PMA_PAGE_SIZE;
+  _pma_state->metadata->arena_end = (uint8_t*)_pma_state->metadata->arena_end + PMA_PAGE_SIZE;
 
   // Add page to dirty list
   _pma_mark_page_dirty(PTR_TO_INDEX(address), offset, status, 1);
@@ -1729,7 +1729,7 @@ _pma_get_new_pages(uint64_t num_pages) {
 
   // Update offset of next open dpage
   _pma_state->metadata->next_offset += bytes;
-  _pma_state->metadata->arena_end += bytes;
+  _pma_state->metadata->arena_end = (uint8_t*)_pma_state->metadata->arena_end + bytes;
 
   // Add allocated pages to dirty list
   _pma_mark_page_dirty(PTR_TO_INDEX(address), offset, FIRST, num_pages);
