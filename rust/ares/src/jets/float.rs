c4e303f262eb67f5a8f5023c84b3767c99fe2de0
Sigilante <neal@urbit.org>
2023-09-22 13:13:20 -0500

    Post ++rs all jets.

--- rust/ares/src/jets/float.rs
+++ rust/ares/src/jets/float.rs
@@ -6,7 +6,7 @@ use crate::jets::util::*;
 use crate::jets::util::test::{assert_jet, assert_jet_err, assert_jet_ubig, assert_nary_jet_ubig, init_stack, A};
 use crate::mem::NockStack;
 use crate::newt::Newt;
-use crate::noun::{Atom, IndirectAtom, Noun, D, T, Cell};
+use crate::noun::{Atom, IndirectAtom, Noun, D, T, Cell, NO, YES};
 use ibig::{UBig, ubig};
 use softfloat_sys::*;
 use std::io::Write;
@@ -68,9 +68,9 @@ pub fn jet_rs_add(
         let mod_r = _set_rounding(r);
 
         softfloat_roundingMode_write_helper(mod_r);
-        let c = _nan_unify(f32_add(dat_a, dat_b));
+        let x = _nan_unify(f32_add(dat_a, dat_b));
 
-        Ok(D(c.v as u64))
+        Ok(D(x.v as u64))
     }
 }
 
@@ -90,9 +90,9 @@ pub fn jet_rs_sub(
         let mod_r = _set_rounding(r);
 
         softfloat_roundingMode_write_helper(mod_r);
-        let c = _nan_unify(f32_sub(dat_a, dat_b));
+        let x = _nan_unify(f32_sub(dat_a, dat_b));
 
-        Ok(D(c.v as u64))
+        Ok(D(x.v as u64))
     }
 }
 
@@ -112,9 +112,9 @@ pub fn jet_rs_mul(
         let mod_r = _set_rounding(r);
 
         softfloat_roundingMode_write_helper(mod_r);
-        let c = _nan_unify(f32_mul(dat_a, dat_b));
+        let x = _nan_unify(f32_mul(dat_a, dat_b));
 
-        Ok(D(c.v as u64))
+        Ok(D(x.v as u64))
     }
 }
 
@@ -134,9 +134,165 @@ pub fn jet_rs_div(
         let mod_r = _set_rounding(r);
 
         softfloat_roundingMode_write_helper(mod_r);
-        let c = _nan_unify(f32_div(dat_a, dat_b));
+        let x = _nan_unify(f32_div(dat_a, dat_b));
 
-        Ok(D(c.v as u64))
+        Ok(D(x.v as u64))
+    }
+}
+
+pub fn jet_rs_sqt(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        eprintln!("»sam: {:?}", sam);
+        let a = sam.as_atom()?.as_direct()?;
+        eprintln!("»a: {:?}", a.as_noun());
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let x = _nan_unify(f32_sqrt(dat_a));
+
+        Ok(D(x.v as u64))
+    }
+}
+
+pub fn jet_rs_fma(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 6)?.as_atom()?.as_direct()?;
+        let c = slot(sam, 7)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let dat_c = softfloat_sys::float32_t { v: c.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let x = _nan_unify(f32_mulAdd(dat_a, dat_b, dat_c));
+        
+        Ok(D(x.v as u64))
+    }
+}
+
+pub fn jet_rs_lth(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 3)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let t = f32_lt(dat_a, dat_b);
+
+        if t { Ok(YES) } else { Ok(NO) }
+    }
+}
+
+pub fn jet_rs_lte(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 3)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let t = f32_le(dat_a, dat_b);
+
+        if t { Ok(YES) } else { Ok(NO) }
+    }
+}
+
+pub fn jet_rs_equ(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 3)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let t = f32_eq(dat_a, dat_b);
+
+        if t { Ok(YES) } else { Ok(NO) }
+    }
+}
+
+pub fn jet_rs_gte(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 3)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let t = f32_le(dat_b, dat_a);
+
+        if t { Ok(YES) } else { Ok(NO) }
+    }
+}
+
+pub fn jet_rs_gth(
+    _stack: &mut NockStack,
+    _newt: &mut Option<&mut Newt>,
+    subject: Noun
+) -> jets::Result {
+    unsafe{
+        let sam = slot(subject, 6)?;
+        let a = slot(sam, 2)?.as_atom()?.as_direct()?;
+        let b = slot(sam, 3)?.as_atom()?.as_direct()?;
+        let r = slot(subject, 30)?.as_atom()?.as_direct()?.data() as u8 as char;
+
+        let dat_a = softfloat_sys::float32_t { v: a.data() as u32};
+        let dat_b = softfloat_sys::float32_t { v: b.data() as u32};
+        let mod_r = _set_rounding(r);
+
+        softfloat_roundingMode_write_helper(mod_r);
+        let t = f32_lt(dat_b, dat_a);
+
+        if t { Ok(YES) } else { Ok(NO) }
     }
 }
 
@@ -158,17 +314,17 @@ mod tests {
         unsafe {
             let mut sam: Vec<Noun> = sam.iter().map(|f| f(stack)).collect();
             let mut ctx: Vec<Noun> = ctx.iter().map(|f| f(stack)).collect();
-            let sam = T(stack, &sam);
-            let ctx = T(stack, &ctx);
+            let sam = if(sam.len() > 1) { T(stack, &sam) } else { sam[0] };
+            eprintln!("sam: {:?}", sam);
+            let ctx = if(ctx.len() > 1) { T(stack, &ctx) } else { ctx[0] };
+            eprintln!("ctx: {:?}", ctx);
             let pay = Cell::new(stack, sam, ctx).as_noun();
+            eprintln!("pay: {:?}", pay);
             let sbj = Cell::new(stack, D(0), pay).as_noun();
-            eprintln!("sam: {:?}\n", sam);
-            eprintln!("ctx: {:?}\n", ctx);
-            eprintln!("pay: {:?}\n", pay);
-            eprintln!("sbj: {:?}\n", sbj);
+            eprintln!("sbj: {:?}", sbj);
             std::io::stderr().flush().unwrap();
             let jet_res = jet(stack, &mut None, sbj).unwrap();
-            eprintln!("jet: {:x}", jet_res.as_atom().expect("msg").as_direct().expect("msg").data());
+            eprintln!("jet: {:x}\n", jet_res.as_atom().expect("msg").as_direct().expect("msg").data());
             std::io::stderr().flush().unwrap();
             assert_noun_eq(stack, jet_res, res);
         }
@@ -192,11 +348,6 @@ mod tests {
 
     fn atom_1_5(_stack: &mut NockStack) -> Noun {
         D(0x3fc00000)
-    }
-
-    // for test (1.1 - 0.8) - 0.3
-    fn atom_0_(_stack: &mut NockStack) -> Noun {
-        D(0x24800000)
     }
 
     fn atom_1_1(_stack: &mut NockStack) -> Noun {
@@ -250,7 +401,7 @@ mod tests {
         assert_jet_in_door(s, jet_rs_sub, &[atom_2, atom_1], &[atom_0, r_sample_n, atom_0], D(0x3f800000));
         assert_jet_in_door(s, jet_rs_sub, &[atom_1_1, atom_0_8], &[atom_0, r_sample_n, atom_0], D(0x3e99999a));
         assert_jet_in_door(s, jet_rs_sub, &[atom_1_1, atom_0_3], &[atom_0, r_sample_n, atom_0], D(0x3f4ccccd));
-    }
+}
 
     #[test]
     fn test_rs_mul() {
@@ -261,6 +412,7 @@ mod tests {
         assert_jet_in_door(s, jet_rs_mul, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], D(0x3f800000));
         assert_jet_in_door(s, jet_rs_mul, &[atom_1, atom_2], &[atom_0, r_sample_n, atom_0], D(0x40000000));
         assert_jet_in_door(s, jet_rs_mul, &[atom_2, atom_2], &[atom_0, r_sample_n, atom_0], D(0x40800000));
+        assert_jet_in_door(s, jet_rs_mul, &[atom_1_5, atom_2], &[atom_0, r_sample_n, atom_0], D(0x40400000));
         assert_jet_in_door(s, jet_rs_mul, &[atom_1_1, atom_0_8], &[atom_0, r_sample_z, atom_0], D(0x3f6147ae));
         assert_jet_in_door(s, jet_rs_mul, &[atom_1_1, atom_0_8], &[atom_0, r_sample_n, atom_0], D(0x3f6147af));
         assert_jet_in_door(s, jet_rs_mul, &[atom_1_1, atom_0_3], &[atom_0, r_sample_n, atom_0], D(0x3ea8f5c3));
@@ -280,4 +432,76 @@ mod tests {
         assert_jet_in_door(s, jet_rs_div, &[atom_1_1, atom_0_3], &[atom_0, r_sample_n, atom_0], D(0x406aaaaa));
         assert_jet_in_door(s, jet_rs_div, &[atom_1_1, atom_0_3], &[atom_0, r_sample_u, atom_0], D(0x406aaaab));
     }
+
+    #[test]
+    fn test_rs_sqt() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_sqt, &[atom_0], &[atom_0, r_sample_n, atom_0], D(0x00000000));
+        assert_jet_in_door(s, jet_rs_sqt, &[atom_1], &[atom_0, r_sample_n, atom_0], D(0x3f800000));
+        assert_jet_in_door(s, jet_rs_sqt, &[atom_2], &[atom_0, r_sample_n, atom_0], D(0x3fb504f3));
+        assert_jet_in_door(s, jet_rs_sqt, &[atom_3], &[atom_0, r_sample_n, atom_0], D(0x3fddb3d7));
+        assert_jet_in_door(s, jet_rs_sqt, &[atom_1_1], &[atom_0, r_sample_n, atom_0], D(0x3f863f5e));
+    }
+
+    #[test]
+    fn test_rs_fma() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_fma, &[atom_1, atom_0, atom_0], &[atom_0, r_sample_n, atom_0], D(0x00000000));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_0, atom_1, atom_1], &[atom_0, r_sample_n, atom_0], D(0x3f800000));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_1, atom_1, atom_1], &[atom_0, r_sample_n, atom_0], D(0x40000000));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_1, atom_2, atom_1], &[atom_0, r_sample_n, atom_0], D(0x40400000));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_2, atom_2, atom_1], &[atom_0, r_sample_n, atom_0], D(0x40a00000));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_1_1, atom_0_8, atom_0_3], &[atom_0, r_sample_n, atom_0], D(0x3f970a3e));
+        assert_jet_in_door(s, jet_rs_fma, &[atom_1_1, atom_0_8, atom_0_3], &[atom_0, r_sample_z, atom_0], D(0x3f970a3d));
+    }
+
+    #[test]
+    fn test_rs_lth() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_lth, &[atom_1, atom_0], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_lth, &[atom_0, atom_1], &[atom_0, r_sample_n, atom_0], YES);
+        assert_jet_in_door(s, jet_rs_lth, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], NO);
+    }
+
+    #[test]
+    fn test_rs_lte() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_lte, &[atom_1, atom_0], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_lte, &[atom_0, atom_1], &[atom_0, r_sample_n, atom_0], YES);
+        assert_jet_in_door(s, jet_rs_lte, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], YES);
+    }
+
+    #[test]
+    fn test_rs_equ() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_equ, &[atom_1, atom_0], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_equ, &[atom_0, atom_1], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_equ, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], YES);
+    }
+
+    #[test]
+    fn test_rs_gte() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_gte, &[atom_1, atom_0], &[atom_0, r_sample_n, atom_0], YES);
+        assert_jet_in_door(s, jet_rs_gte, &[atom_0, atom_1], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_gte, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], YES);
+    }
+
+    #[test]
+    fn test_rs_gth() {
+        let s = &mut init_stack();
+
+        assert_jet_in_door(s, jet_rs_gth, &[atom_1, atom_0], &[atom_0, r_sample_n, atom_0], YES);
+        assert_jet_in_door(s, jet_rs_gth, &[atom_0, atom_1], &[atom_0, r_sample_n, atom_0], NO);
+        assert_jet_in_door(s, jet_rs_gth, &[atom_1, atom_1], &[atom_0, r_sample_n, atom_0], NO);
+    }
+
+
+
 }
