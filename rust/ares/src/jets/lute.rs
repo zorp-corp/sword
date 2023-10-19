32ea7e95d0e620a05b2d6adde2781582a81e4aaf
Matthew LeVan <matthew@coeli.network>
2023-10-19 09:58:56 -0400

    wip: `++{fine,fish,fuse}:ut`

--- rust/ares/src/jets/lute.rs
+++ rust/ares/src/jets/lute.rs
@@ -11,6 +11,151 @@ crate::gdb!();
 
 /*
 u3_noun
+u3wfu_fuse(u3_noun cor)
+{
+  u3_noun bat, sut, ref, van;
+
+  if (  (c3n == u3r_mean(cor, u3x_sam, &ref, u3x_con, &van, 0))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    u3_weak vet = u3r_at(u3qfu_van_vet, van);
+    c3_m  fun_m = 141 + c3__fuse + ((!!vet) << 8);
+    u3_noun key = u3z_key_3(fun_m, sut, ref, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(cor), u3k(u3x_at(u3x_bat, cor)));
+      return u3z_save(key, pro);
+    }
+  }
+}
+*/
+pub fn jet_fuse(context: &mut Context, subject: Noun) -> Result {
+    let ref_ = slot(subject, 6)?;
+    let van = slot(subject, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let flag = if unsafe { vet.raw_equals(D(0)) } { 0 } else { 1 };
+    let fun = (141 + tas!(b"fuse")) + (flag << 8);
+    let mut key = T(context.stack, &[D(fun), sut, ref_, bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, subject, slot(subject, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    context.cache.insert(context.stack, &mut key, pro);
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
+
+/*
+u3_noun
+u3wfu_fish(u3_noun cor)
+{
+  u3_noun bat, sut, axe, van;
+
+  if (  (c3n == u3r_mean(cor, u3x_sam, &axe, u3x_con, &van, 0))
+     || (c3n == u3ud(axe))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    u3_weak vet = u3r_at(u3qfu_van_vet, van);
+    c3_m  fun_m = 141 + c3__fish + ((!!vet) << 8);
+    u3_noun key = u3z_key_3(fun_m, sut, axe, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(cor), u3k(u3x_at(u3x_bat, cor)));
+      return u3z_save(key, pro);
+    }
+  }
+}
+*/
+pub fn jet_fish(context: &mut Context, subject: Noun) -> Result {
+    let axe = slot(subject, 6)?.as_atom()?;
+    let van = slot(subject, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let flag = if unsafe { vet.raw_equals(D(0)) } { 0 } else { 1 };
+    let fun = (141 + tas!(b"fish")) + (flag << 8);
+    let mut key = T(context.stack, &[D(fun), sut, axe.as_noun(), bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, subject, slot(subject, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    context.cache.insert(context.stack, &mut key, pro);
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
+
+/*
+u3_noun
+u3qf_fine(u3_noun fuv,
+          u3_noun lup,
+          u3_noun mar)
+{
+  if ( (c3__void == lup) || (c3__void == mar) ) {
+    return c3__void;
+  } else {
+    return u3nq(c3__fine,
+                u3k(fuv),
+                u3k(lup),
+                u3k(mar));
+  }
+}
+*/
+pub fn jet_fine(context: &mut Context, subject: Noun) -> Result {
+    let fuv = slot(subject, 12)?;
+    let lup = slot(subject, 26)?.as_direct()?;
+    let mar = slot(subject, 27)?.as_direct()?;
+
+    if tas!(b"void") == lup.data() || tas!(b"void") == mar.data() {
+        Ok(D(tas!(b"void")))
+    } else {
+        Ok(T(context.stack, &[D(tas!(b"fine")), fuv, lup.as_noun(), mar.as_noun()]))
+    }
+}
+
+/*
+u3_noun
 u3wfu_crop(u3_noun cor)
 {
   u3_noun bat, sut, ref, van;
@@ -38,7 +183,6 @@ u3wfu_crop(u3_noun cor)
   }
 }
 */
-
 pub fn jet_crop(context: &mut Context, subject: Noun) -> Result {
     let ref_ = slot(subject, 6)?;
     let van = slot(subject, 7)?;
