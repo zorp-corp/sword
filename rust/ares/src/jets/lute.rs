9999d1f83739ce010f2052fe9e2d40cf758894b8
Matthew LeVan <matthew@coeli.network>
2023-10-19 10:53:24 -0400

    wip: `++{rest,nest,mull,mint}:ut` jets

--- rust/ares/src/jets/lute.rs
+++ rust/ares/src/jets/lute.rs
@@ -5,9 +5,272 @@ use crate::interpreter::{Context, interpret};
 use crate::jets::util::*;
 use crate::jets::JetErr::*;
 use crate::jets::Result;
-use crate::noun::{Noun, D, T};
+use crate::noun::{Noun, D, T, YES, NO};
 
 crate::gdb!();
+
+/*
+u3_noun
+u3wfu_rest(u3_noun cor)
+{
+  u3_noun bat, sut, leg, van;
+
+  if ( (c3n == u3r_mean(cor, u3x_sam, &leg, u3x_con, &van, 0))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    u3_weak vet = u3r_at(u3qfu_van_vet, van);
+    c3_m  fun_m = 141 + c3__rest + ((!!vet) << 8);
+    u3_noun key = u3z_key_3(fun_m, sut, leg, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(cor), u3k(u3x_at(u3x_bat, cor)));
+      return u3z_save(key, pro);
+    }
+  }
+}
+*/
+pub fn jet_rest(context: &mut Context, subject: Noun) -> Result {
+    let leg = slot(subject, 6)?;
+    let van = slot(subject, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let flag = if unsafe { vet.raw_equals(D(0)) } { 0 } else { 1 };
+    let fun = (141 + tas!(b"rest")) + (flag << 8);
+    let mut key = T(context.stack, &[D(fun), sut, leg, bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, subject, slot(subject, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    context.cache.insert(context.stack, &mut key, pro);
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
+
+/*
+u3_noun
+u3wfu_nest_dext(u3_noun dext_core)
+{
+  u3_noun nest_in_core, nest_core;
+  u3_noun bat, sut, ref, van, seg, reg, gil;
+
+  if (  (u3_none == (nest_in_core = u3r_at(3, dext_core)))
+     || (c3n == u3r_mean(nest_in_core, u3x_sam_2, &seg,
+                                       u3x_sam_6, &reg,
+                                       u3x_sam_7, &gil,
+                                       u3x_con, &nest_core, 0))
+     || (c3n == u3r_mean(nest_core, u3x_sam_3, &ref,
+                                    u3x_con, &van, 0))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    u3_weak vet = u3r_at(u3qfu_van_vet, van);
+    c3_m  fun_m = 141 + c3__dext + ((!!vet) << 8);
+    u3_noun key = u3z_key_3(fun_m, sut, ref, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(dext_core), u3k(u3x_at(u3x_bat, dext_core)));
+
+      if ( ((c3y == pro) && (u3_nul == reg)) ||
+           ((c3n == pro) && (u3_nul == seg)) )
+      {
+        return u3z_save(key, pro);
+      }
+      else {
+        u3z(key);
+        return pro;
+      }
+    }
+  }
+}
+*/
+pub fn jet_nest(context: &mut Context, subject: Noun) -> Result {
+    let nest_in_core = slot(subject, 3)?;
+    let seg = slot(nest_in_core, 12)?;
+    let reg = slot(nest_in_core, 26)?;
+    // let gil = slot(nest_in_core, 27)?;
+    let nest_core = slot(nest_in_core, 7)?;
+    let ref_ = slot(nest_core, 13)?;
+    let van = slot(nest_core, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let flag = if unsafe { vet.raw_equals(D(0)) } { 0 } else { 1 };
+    let fun = (141 + tas!(b"dext")) + (flag << 8);
+    let mut key = T(context.stack, &[D(fun), sut, ref_, bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, nest_in_core, slot(nest_in_core, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    if (unsafe { pro.raw_equals(YES) && reg.raw_equals(D(0)) } ) ||
+                       (unsafe { pro.raw_equals(NO)  && seg.raw_equals(D(0)) } ) {
+                        context.cache.insert(context.stack, &mut key, pro);
+                    }
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
+
+/*
+u3_noun
+u3wfu_mull(u3_noun cor)
+{
+  u3_noun bat, sut, gol, dox, gen, van;
+
+  if (  (c3n == u3r_mean(cor, u3x_sam_2, &gol,
+                              u3x_sam_6, &dox,
+                              u3x_sam_7, &gen,
+                              u3x_con, &van, 0))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    u3_weak vet = u3r_at(u3qfu_van_vet, van);
+    c3_m  fun_m = 141 + c3__mull + ((!!vet) << 8);
+    u3_noun key = u3z_key_5(fun_m, sut, gol, dox, gen, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(cor), u3k(u3x_at(u3x_bat, cor)));
+      return u3z_save(key, pro);
+    }
+  }
+}
+*/
+pub fn jet_mull(context: &mut Context, subject: Noun) -> Result {
+    let gol = slot(subject, 12)?;
+    let dox = slot(subject, 26)?;
+    let gen = slot(subject, 27)?;
+    let van = slot(subject, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let flag = if unsafe { vet.raw_equals(D(0)) } { 0 } else { 1 };
+    let fun = (141 + tas!(b"mull")) + (flag << 8);
+    let mut key = T(context.stack, &[D(fun), sut, gol, dox, gen, bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, subject, slot(subject, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    context.cache.insert(context.stack, &mut key, pro);
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
+
+/*
+u3_noun
+u3wfu_mint(u3_noun cor)
+{
+  u3_noun bat, sut, gol, gen, van;
+
+  if (  (c3n == u3r_mean(cor, u3x_sam_2, &gol,
+                              u3x_sam_3, &gen,
+                              u3x_con, &van, 0))
+     || (u3_none == (bat = u3r_at(u3x_bat, van)))
+     || (u3_none == (sut = u3r_at(u3x_sam, van))) )
+  {
+    return u3m_bail(c3__fail);
+  }
+  else {
+    c3_m  fun_m = 141 + c3__mint;
+    u3_noun vet = u3r_at(u3qfu_van_vet, van);
+    u3_noun key = u3z_key_5(fun_m, vet, sut, gol, gen, bat);
+    u3_weak pro = u3z_find(key);
+
+    if ( u3_none != pro ) {
+      u3z(key);
+      return pro;
+    }
+    else {
+      pro = u3n_nock_on(u3k(cor), u3k(u3x_at(u3x_bat, cor)));
+      return u3z_save(key, pro);
+    }
+  }
+}
+*/
+pub fn jet_mint(context: &mut Context, subject: Noun) -> Result {
+    let gol = slot(subject, 12)?;
+    let gen = slot(subject, 13)?;
+    let van = slot(subject, 7)?;
+    let bat = slot(van , 2)?;
+    let sut = slot(van , 6)?; // why does vere bail:fail if any of these are none?
+
+    let vet = slot(van, 59)?;
+    let fun = 141 + tas!(b"mint");
+    let mut key = T(context.stack, &[D(fun), vet, sut, gol, gen, bat]);
+    let pro = context.cache.lookup(context.stack, &mut key);
+
+    match pro {
+        Some(pro) => Ok(pro),
+        None => {
+            let res = interpret(context, subject, slot(subject, 2)?);
+            match res {
+                Err(_) => {
+                    Err(Deterministic)
+                },
+                Ok(pro) => {
+                    context.cache.insert(context.stack, &mut key, pro);
+                    Ok(pro)
+                }
+            }
+        }
+    }
+}
 
 /*
 u3_noun
