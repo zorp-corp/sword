0b554c948b6d1057eff5cb097cd2a4208a69af17
Alex Shelkovnykov <a@ashelkov.com>
2023-10-27 07:47:09 +0100

    Add jets for +mute & +mule

--- rust/ares/src/jets/nock.rs
+++ rust/ares/src/jets/nock.rs
@@ -4,7 +4,7 @@ use crate::hamt::Hamt;
 use crate::interpreter::{interpret, Context, Error};
 use crate::jets::util::slot;
 use crate::jets::{JetErr, Result};
-use crate::noun::{Noun, D, T};
+use crate::noun::{Cell, Noun, D, NO, T, YES};
 
 crate::gdb!();
 
@@ -80,6 +80,10 @@ pub fn jet_mole(context: &mut Context, subject: Noun) -> Result {
     jet_mure(context, subject)
 }
 
+pub fn jet_mule(context: &mut Context, subject: Noun) -> Result {
+    jet_mute(context, subject)
+}
+
 pub fn jet_mure(context: &mut Context, subject: Noun) -> Result {
     let tap = slot(subject, 6)?;
     let fol = util::slam_gate_fol(&mut context.stack);
@@ -96,6 +100,36 @@ pub fn jet_mure(context: &mut Context, subject: Noun) -> Result {
             // crash must have come from a senior virtualization context.
             Error::NonDeterministic(_) | Error::ScryCrashed(_) => Err(JetErr::Fail(error)),
             Error::Deterministic(_) | Error::ScryBlocked(_) => Ok(D(0)),
+        },
+    }
+}
+
+pub fn jet_mute(context: &mut Context, subject: Noun) -> Result {
+    let tap = slot(subject, 6)?;
+    let fol = util::slam_gate_fol(&mut context.stack);
+    let scry = util::pass_thru_scry(&mut context.stack);
+
+    context.scry_stack = T(&mut context.stack, &[scry, context.scry_stack]);
+    match interpret(context, tap, fol) {
+        Ok(res) => {
+            context.scry_stack = context.scry_stack.as_cell()?.tail();
+            Ok(T(&mut context.stack, &[YES, res]))
+        }
+        Err(error) => match error {
+            // Since we are using the pass-through scry handler, we know for a fact that a scry
+            // crash must have come from a senior virtualization context.
+            Error::NonDeterministic(_) | Error::ScryCrashed(_) => Err(JetErr::Fail(error)),
+            Error::ScryBlocked(path) => {
+                //  XX: Need to check that result is actually of type path
+                //      return [[%leaf "mute.hunk"] ~] if not
+                let bon = util::smyt(&mut context.stack, path)?;
+                Ok(T(&mut context.stack, &[NO, bon, D(0)]))
+            }
+            Error::Deterministic(trace) => {
+                let ton = Cell::new(&mut context.stack, D(2), trace);
+                let tun = util::mook(context, ton, false)?;
+                Ok(T(&mut context.stack, &[NO, tun.tail()]))
+            }
         },
     }
 }
