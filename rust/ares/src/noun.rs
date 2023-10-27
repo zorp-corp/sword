46275f51839f7532ed8265a5b44b8166596563e4
Edward Amsden <edward@blackriversoft.com>
2023-10-27 14:04:56 -0500

    noun: lost paren

--- rust/ares/src/noun.rs
+++ rust/ares/src/noun.rs
@@ -39,7 +39,7 @@ const FORWARDING_MASK: u64 = CELL_MASK;
 /** Loobeans */
 pub const YES: Noun = D(0);
 pub const NO: Noun = D(1);
-pub const NONE: Noun = unsafe { DirectAtom::new_unchecked(tas!(b"MORMAGIC").as_noun() };
+pub const NONE: Noun = unsafe { DirectAtom::new_unchecked(tas!(b"MORMAGIC")).as_noun() };
 
 #[cfg(feature = "check_acyclic")]
 #[macro_export]
