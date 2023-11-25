6c0727fb65be5ac65cb50f6320ebbf77eb77e14f
Matthew LeVan <matthew@coeli.network>
2023-11-25 10:54:50 -0500

    load: clarify `load_cg_trap` comment

--- rust/ares/src/load.rs
+++ rust/ares/src/load.rs
@@ -6,7 +6,7 @@ use crate::serialization::cue_bytes;
 use ares_macros::tas;
 use std::include_bytes;
 
-/// Load a trap and the formula to kick it and get the codegen core
+/// Return a (formula, trap) that can be kicked to get the codegen core
 pub fn load_cg_trap(stack: &mut NockStack) -> Result<(Noun, Noun), Error> {
     let cg_bytes = include_bytes!("../bin/cg.jam");
     let cg_noun = cue_bytes(stack, cg_bytes);
