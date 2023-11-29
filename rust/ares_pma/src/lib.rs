add377b0076a4dcd2ca25d0fc59f653ddefb1167
Edward Amsden <edward@blackriversoft.com>
2023-11-29 10:36:19 -0600

    pma: export bindgen bindings from lib

--- rust/ares_pma/src/lib.rs
+++ rust/ares_pma/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
