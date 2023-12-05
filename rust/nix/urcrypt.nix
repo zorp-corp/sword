4451a342c492859eb670ea3210b62ca24913204e
Matthew LeVan <matthew@coeli.network>
2023-12-05 10:54:18 -0500

    build: support latest `urcrypt`

--- rust/nix/urcrypt.nix
+++ rust/nix/urcrypt.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, pkg-config, openssl, cryptopp, secp256k1, libaes_siv }:
-let rev = "375fa7e6a730d8aa517ca981b2b7b505bf4e1103";
+{ stdenv, fetchFromGitHub, autoreconfHook, autoconf-archive, pkg-config, openssl, cryptopp, secp256k1, libaes_siv }:
+let rev = "43479c3262a11e20da5f6218f3b0b3d63931ceea";
 in stdenv.mkDerivation {
   pname = "urcrypt";
   version = "git-${rev}";
@@ -7,14 +7,14 @@ in stdenv.mkDerivation {
     inherit rev;
     owner = "urbit";
     repo = "urcrypt";
-    hash = "sha256:1c3cqmwr5mys4v9y0834hxqfr6aynm2gav7730bjzfvrdc21ijqa";
+    hash = "sha256-GkhqvhDyhsdzjWpR8uqmhdRdhxdpmLGWXtIUZPAbWZs=";
   };
 
-  preConfigure = ''
-    ./autogen.sh
-    '';
+  # preConfigure = ''
+  #   ./autogen.sh
+  #   '';
 
-  nativeBuildInputs = [autoreconfHook pkg-config];
+  nativeBuildInputs = [autoreconfHook autoconf-archive pkg-config];
 
   buildInputs = [openssl cryptopp secp256k1 libaes_siv];
 }
