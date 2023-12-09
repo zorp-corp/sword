34fd4651c66d0649947cd99863bf80bc7d185937
Matthew LeVan <matthew@coeli.network>
2023-12-08 20:06:36 -0500

    preserve cache, cold, warm in `with_stack_frame`

--- rust/ares/src/interpreter.rs
+++ rust/ares/src/interpreter.rs
@@ -300,6 +300,9 @@ impl Context {
         self.stack.frame_push(slots);
         let mut ret = f(self);
         ret.preserve(&mut self.stack);
+        self.stack.preserve(&mut self.cache);
+        self.stack.preserve(&mut self.cold);
+        self.stack.preserve(&mut self.warm);
         self.stack.frame_pop();
         ret
     }
