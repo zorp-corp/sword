7b2684aa26e1d6e6a627d0a74a17d12b5531e41f
Edward Amsden <edward@blackriversoft.com>
2023-12-18 10:46:34 -0600

    jets: jet +turn

--- rust/ares/src/jets/list.rs
+++ rust/ares/src/jets/list.rs
@@ -1,9 +1,9 @@
 /** Text processing jets
  */
-use crate::interpreter::Context;
+use crate::interpreter::{interpret, Context, Error};
 use crate::jets::util::slot;
-use crate::jets::Result;
-use crate::noun::{Noun, D};
+use crate::jets::{JetErr, Result};
+use crate::noun::{Cell, Noun, D, T};
 
 crate::gdb!();
 
@@ -22,6 +22,72 @@ pub fn jet_zing(context: &mut Context, subject: Noun) -> Result {
     let stack = &mut context.stack;
 
     util::zing(stack, list)
+}
+
+pub fn jet_turn(context: &mut Context, subject: Noun) -> Result {
+    let sample = slot(subject, 6)?;
+    let mut list = slot(sample, 2)?;
+    let mut gate = slot(sample, 3)?;
+    let mut gate_battery = slot(gate, 2)?;
+    let gate_context = slot(gate, 7)?;
+    let mut res = D(0);
+    let mut dest: *mut Noun = &mut res; // Mutable pointer because we cannot guarantee initialized
+
+    // Since the gate doesn't change, we can do a single jet check and use that through the whole
+    // loop
+    if let Some((jet, _path)) =
+        context
+            .warm
+            .find_jet(&mut context.stack, &mut gate, &mut gate_battery)
+    {
+        loop {
+            if let Ok(list_cell) = list.as_cell() {
+                list = list_cell.tail();
+                let element_subject = T(
+                    &mut context.stack,
+                    &[gate_battery, list_cell.head(), gate_context],
+                );
+                unsafe {
+                    let (new_cell, new_mem) = Cell::new_raw_mut(&mut context.stack);
+                    (*new_mem).head = jet(context, element_subject)?;
+                    *dest = new_cell.as_noun();
+                    dest = &mut (*new_mem).tail;
+                }
+            } else {
+                if unsafe { !list.raw_equals(D(0)) } {
+                    return Err(JetErr::Fail(Error::Deterministic(D(0))));
+                }
+                unsafe {
+                    *dest = D(0);
+                };
+                return Ok(res);
+            }
+        }
+    } else {
+        loop {
+            if let Ok(list_cell) = list.as_cell() {
+                list = list_cell.tail();
+                let element_subject = T(
+                    &mut context.stack,
+                    &[gate_battery, list_cell.head(), gate_context],
+                );
+                unsafe {
+                    let (new_cell, new_mem) = Cell::new_raw_mut(&mut context.stack);
+                    (*new_mem).head = interpret(context, element_subject, gate_battery)?;
+                    *dest = new_cell.as_noun();
+                    dest = &mut (*new_mem).tail;
+                }
+            } else {
+                if unsafe { !list.raw_equals(D(0)) } {
+                    return Err(JetErr::Fail(Error::Deterministic(D(0))));
+                }
+                unsafe {
+                    *dest = D(0);
+                };
+                return Ok(res);
+            }
+        }
+    }
 }
 
 pub mod util {
