561701aaf1882849a5c27bf9f546d0ce11a8166d
Matthew LeVan <matthew@coeli.network>
2023-12-18 12:55:13 -0500

    wip: `shar`

--- rust/ares_crypto/src/ed25519.rs
+++ rust/ares_crypto/src/ed25519.rs
@@ -1,6 +1,6 @@
-use rand::{rngs::StdRng, SeedableRng};
+use curve25519_dalek::{edwards::CompressedEdwardsY, EdwardsPoint, MontgomeryPoint};
 use ed25519_dalek::{SigningKey, VerifyingKey};
-use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
+use x25519_dalek::{PublicKey, StaticSecret};
 
 /// Generate a public key from the given seed and write it to the given output slice.
 pub fn ac_ed_puck(seed: &mut [u8; 32], out: &mut [u8; 32]) {
@@ -13,11 +13,16 @@ pub fn ac_ed_puck(seed: &mut [u8; 32], out: &mut [u8; 32]) {
 /// the keypair generate from the given seed, writing the resulting shared key
 /// to the given output slice.
 pub fn ac_ed_shar(public: &[u8; 32], seed: &[u8; 32], out: &mut [u8; 32]) {
-    // let self_key = SigningKey::from_bytes(seed);
+    let self_key = SigningKey::from_bytes(seed);
+    let self_secret = StaticSecret::from(self_key.to_bytes());
 
-    // let other_key = PublicKey::from(*public);
-    // let shared_secret = self_key.diffie_hellman(&other_key);
-    // *out = shared_secret.to_bytes();
+    if let Ok(compressed_ed_pt) = CompressedEdwardsY::from_slice(public) {
+        if let Some(ed_pt) = compressed_ed_pt.decompress() {
+            let public_key = PublicKey::from(ed_pt.to_montgomery().to_bytes());
+            let shared_secret = self_secret.diffie_hellman(&public_key);
+            *out = shared_secret.to_bytes();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -61,17 +66,17 @@ mod ucrypt_tests {
 
     #[test]
     fn test_ed_shar() {
-        let seed_src =
-            &ubig!(_0x4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb)
-                .to_le_bytes();
-        let mut seed: [u8; 32] = [0; 32];
-        seed.copy_from_slice(seed_src);
-
         let public_key_src =
             &ubig!(_0xd75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a)
                 .to_le_bytes();
         let mut public_key: [u8; 32] = [0; 32];
         public_key.copy_from_slice(public_key_src);
+
+        let seed_src =
+            &ubig!(_0x4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb)
+                .to_le_bytes();
+        let mut seed: [u8; 32] = [0; 32];
+        seed.copy_from_slice(seed_src);
 
         let mut uc_out: [u8; 32] = [0; 32];
         unsafe { urcrypt_ed_shar(public_key.as_ptr(), seed.as_ptr(), uc_out.as_mut_ptr()) };
