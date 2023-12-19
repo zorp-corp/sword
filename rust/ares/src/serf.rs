d2a5c5aaeb1e7da92da96f352f2e845fc14e5ac7
Alex Shelkovnykov <a@ashelkov.com>
2023-12-19 16:47:31 -0300

    serf: remove debug eprintln

--- rust/ares/src/serf.rs
+++ rust/ares/src/serf.rs
@@ -303,7 +303,6 @@ fn peek(context: &mut Context, ovo: Noun) -> Noun {
 }
 
 fn goof(context: &mut Context, traces: Noun) -> Noun {
-    // eprintln!("serf: goof: traces: {}", traces);
     let trace = zing(&mut context.nock_context.stack, traces).expect("serf: goof: zing failed");
     let tone = Cell::new(&mut context.nock_context.stack, D(2), trace);
     let tang = mook(&mut context.nock_context, tone, false)
