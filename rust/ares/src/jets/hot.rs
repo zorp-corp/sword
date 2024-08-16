e87ef042758e4477fb532c0c2b28fe1a64d18bce
litlep-nibbyt <9344275+litlep-nibbyt@users.noreply.github.com>
2024-08-16 16:39:01 -0400

    disable turn

--- rust/ares/src/jets/hot.rs
+++ rust/ares/src/jets/hot.rs
@@ -95,11 +95,6 @@ pub const URBIT_HOT_STATE: &[HotEntry] = &[
         jet_lent,
     ),
     (
-        &[K_139, Left(b"one"), Left(b"two"), Left(b"turn")],
-        1,
-        jet_turn,
-    ),
-    (
         &[K_139, Left(b"one"), Left(b"two"), Left(b"zing")],
         1,
         jet_zing,
