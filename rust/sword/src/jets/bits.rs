9531b913901f085fe7e94e3807a14d870ad2d760
litlep-nibbyt <9344275+litlep-nibbyt@users.noreply.github.com>
2024-09-16 17:45:05 -0400

    checkpoint

--- rust/sword/src/jets/bits.rs
+++ rust/sword/src/jets/bits.rs
@@ -69,39 +69,51 @@ pub fn jet_cut(context: &mut Context, subject: Noun) -> Result {
     Ok(new_indirect.as_noun())
 }
 
-//++  sew                                                 ::  stitch into
-//  ~/  %sew
-//  |=  [a=bloq [b=step c=step d=@] e=@]
-//  ^-  @
-//  %+  add
-//    (can a b^e c^d ~)
-//  =/  f  [a (add b c)]
-//  (lsh f (rsh f e))
-//
-//  create the the new atom to stich from the right side
-//  overwrite the bits in the receiver atom e by doing the lsh and rsh
-//
-// There is a simpler way to do this as a bitslice
 pub fn jet_sew(context: &mut Context, subject: Noun) -> Result {
     let sam = slot(subject, 6)?;
     let bloq = slot(sam, 2)?.as_atom()?.as_u64()? as usize;
-
-    let sell = slot(sam, 6)?;
     let e = slot(sam, 7)?.as_atom()?;
-    let b = slot(sell, 2)?.as_atom()?.as_u64()? as usize;
-    let step = slot(sell, 6)?.as_atom()?.as_u64()? as usize;
-    let d = slot(sell, 7)?.as_atom()?;
-    eprintln!("sew: donor: {:?}, receiver: {:?}", d.as_bitslice(), e.as_bitslice());
 
-    unsafe {
-        let (mut new_indirect, new_slice) =
-            IndirectAtom::new_raw_mut_bitslice(&mut context.stack, e.size());
-        new_slice.copy_from_bitslice(&e.as_bitslice());
-        chop(bloq, 0, step, b, new_slice, d.as_bitslice())?;
-        Ok(new_indirect.normalize_as_atom().as_noun())
+    let e_bitslice = e.as_bitslice();
+    eprintln!("e_bitslice: {:?}", e_bitslice);
+
+    let bcd = slot(sam, 6)?;
+    let b = slot(bcd, 2)?.as_atom()?.as_u64()? as usize;
+    let step = slot(bcd, 6)?.as_atom()?.as_u64()? as usize;
+    let d = slot(bcd, 7)?.as_atom()?;
+    let donor = d.as_bitslice();
+
+    let offset = b << bloq;
+    let step_b = step << bloq;
+    let end_b = offset + step_b;
+
+    eprintln!("e_bitslice len: {:?}", e_bitslice.len());
+
+    let d_offset = if step_b > donor.len() {
+        donor.len()
     }
+    else{ step_b };
 
+    // e = 0xdead.beef
+    // d = 0x0
+    // a = 2
+    // b = 1
+    // c = 2
+    // 0xdead.b00f
+    unsafe {
+        let (mut dest_indirect, dest) =
+            IndirectAtom::new_raw_mut_bitslice(&mut context.stack, end_b);
+        dest[..e_bitslice.len()].copy_from_bitslice(&e_bitslice);
+
+        let (mut dee_indirect, dee) =
+            IndirectAtom::new_raw_mut_bitslice(&mut context.stack, step_b);
+        dee[0..d_offset].copy_from_bitslice(&donor[0..d_offset]);
+
+        dest[offset..offset + dee.len()].copy_from_bitslice(&dee);
+        Ok(dest_indirect.normalize_as_atom().as_noun())
+    }
 }
+
 pub fn jet_end(context: &mut Context, subject: Noun) -> Result {
     let arg = slot(subject, 6)?;
     let (bloq, step) = bite(slot(arg, 2)?)?;
@@ -831,10 +843,18 @@ mod tests {
     fn test_sew() {
         let c = &mut init_context();
         let (a0, a24, _a63, a96, a128) = atoms(&mut c.stack);
+        let bloq2 = D(0);
         let bloq16 = D(2);
-        let bcd = T(&mut c.stack, &[D(3), D(2), D(0xbaa)]);
-        let sam = T(&mut c.stack, &[bloq16, bcd, a96]);
-        let res = A(&mut c.stack, &ubig!(0xfaceb00c15deadbeef1aa456));
+
+        //D(0x876543)
+        //let bcd = T(&mut c.stack, &[D(7), D(2), D(0xbad)]);
+        //let sam = T(&mut c.stack, &[bloq16, bcd, a24]);
+        //let res = A(&mut c.stack, &ubig!(0xad0876543));
+        //assert_jet(c, jet_sew, sam, res);
+
+        let bcd = T(&mut c.stack, &[D(1), D(2), D(0)]);
+        let sam = T(&mut c.stack, &[bloq16, bcd, D(0xdeadbeef)]);
+        let res = A(&mut c.stack, &ubig!(0xdeadb00f));
         assert_jet(c, jet_sew, sam, res);
     }
 
